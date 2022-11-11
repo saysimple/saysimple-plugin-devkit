e345c83f6e6a529fb3c45d67ed0229b3a2a0bff8
Marcello Honnebier <29-marcello@gitlab.just-internet.nl>
2022-11-11 17:08:13 +0100

    Remove ignored files

--- src/data/contacts/index.ts
+++
@@ -1,6 +0,0 @@
-import { convertFilesToMap } from "@/data/convertFilesToMap";
-import { ContactsInterface } from "@/types/contacts.interface";
-
-const context = require.context("./", false, /.+\.json$/);
-
-export const contacts = convertFilesToMap<ContactsInterface>(context);
--- src/data/users/index.ts
+++
@@ -1,6 +0,0 @@
-import { UsersInterface } from "@/types/users.interface";
-import { convertFilesToMap } from "@/data/convertFilesToMap";
-
-const context = require.context("./", false, /\.json$/);
-
-export const users = convertFilesToMap<UsersInterface>(context);
